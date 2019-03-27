3a5add7a950fcfb1c4db423c6c84849df8c8e454
Feliciano Lima <fsfelicianosantana@gmail.com>
2019-03-27 19:07:06 -0300

    Add pic upload with Telegram

--- telegram_uploader/apps.py
+++ telegram_uploader/apps.py
@@ -1,7 +1,8 @@
 import sys
 import logging
+import shutil
 from django.apps import AppConfig
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # .env support
 import environ
@@ -24,8 +25,18 @@ class TelegramUploaderConfig(AppConfig):
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
         start_handler = CommandHandler('start', start)
         dispatcher.add_handler(start_handler)
+        photo_handler = MessageHandler(Filters.photo, photo)
+        dispatcher.add_handler(photo_handler)
         updater.start_polling()
 
 def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
+def photo(update, context):
+    from photos.models import Photo
+    out = update.message.photo[-1].get_file().download()
+    path = f"media/images/{out}"
+    shutil.move(out, path)
+    image = f"images/{out}"
+    Photo.objects.create(title=out, image=image)
+    context.bot.send_message(chat_id=update.message.chat_id, text="Photo uploaded!")
